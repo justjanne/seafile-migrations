5c9396ca1cc7a4cc7a3d509e516d41913dc72fbd
Janne Mareike Koschinski <mail@justjanne.de>
2026-04-24 00:57:40 +0200

    cleanup

--- liquibase/test-pgsql.sh
+++ liquibase/test-pgsql.sh
@@ -3,5 +3,5 @@
 PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS ccnet; CREATE DATABASE ccnet;"
 liquibase --changelog-file=changelog.yaml --url="jdbc:postgresql://127.0.0.1:5432" --username="postgres" --password="password" --search-path="ccnet" update
 
-PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS seafile; CREATE DATABASE seafile;"
-liquibase --changelog-file=changelog.yaml --url="jdbc:postgresql://127.0.0.1:5432" --username="postgres" --password="password" --search-path="seafile" update
+#PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS seafile; CREATE DATABASE seafile;"
+#liquibase --changelog-file=changelog.yaml --url="jdbc:postgresql://127.0.0.1:5432" --username="postgres" --password="password" --search-path="seafile" update
