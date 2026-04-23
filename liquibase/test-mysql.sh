19601d0fb2e0799266bb584b75fd249b0d4af4e1
Janne Mareike Koschinski <mail@justjanne.de>
2026-04-24 01:06:13 +0200

    cleanup

--- liquibase/test-mysql.sh
+++ liquibase/test-mysql.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-mariadb -h 127.0.0.1 -u root -ppassword -e "DROP DATABASE IF EXISTS ccnet; CREATE DATABASE ccnet;"
-liquibase --changelog-file=changelog.yaml --url="jdbc:mariadb://127.0.0.1:3306/ccnet" --username="root" --password="password" --search-path="ccnet" update
-
-#mariadb -h 127.0.0.1 -u root -ppassword -e "DROP DATABASE IF EXISTS seafile; CREATE DATABASE seafile;"
-#liquibase --changelog-file=changelog.yaml --url="jdbc:mariadb://127.0.0.1:3306/seafile" --username="root" --password="password" --search-path="seafile" update
+LIQUIBASE_ARGS="--url='jdbc:mariadb://127.0.0.1:3306/$1' --username='root' --password='password' --search-path='$1'"
+mariadb -h 127.0.0.1 -u root -ppassword -e "DROP DATABASE IF EXISTS $1; CREATE DATABASE $1;"
+liquibase $LIQUIBASE_ARGS update --changelog-file=changelog.yaml
+liquibase $LIQUIBASE_ARGS snapshot --output-file=../test/$1-mysql.json
--- liquibase/test-pgsql.sh
+++ liquibase/test-pgsql.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS ccnet; CREATE DATABASE ccnet;"
-liquibase --changelog-file=changelog.yaml --url="jdbc:postgresql://127.0.0.1:5432" --username="postgres" --password="password" --search-path="ccnet" update
-
-#PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS seafile; CREATE DATABASE seafile;"
-#liquibase --changelog-file=changelog.yaml --url="jdbc:postgresql://127.0.0.1:5432" --username="postgres" --password="password" --search-path="seafile" update
+LIQUIBASE_ARGS="--url='jdbc:postgresql://127.0.0.1:5432/$1' --username='postgres' --password='password' --search-path='$1'"
+PGPASSWORD=password psql -h 127.0.0.1 -U postgres -c "DROP DATABASE IF EXISTS $1; CREATE DATABASE $1;"
+liquibase $LIQUIBASE_ARGS update --changelog-file=changelog.yaml
+liquibase $LIQUIBASE_ARGS snapshot --output-file=../test/$1-pgsql.json
