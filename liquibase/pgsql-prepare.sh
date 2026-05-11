5bb76f8fa8ed57cf4f3afe47e6b380bd67eafd9e
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-11 20:21:58 +0200

    add organization

--- liquibase/pgsql-prepare.sh
+++ liquibase/pgsql-prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in ldapconfig LDAPConfig ldapusers organization orgfileextwhitelist orggroup orguser userrole; do
+for table in ldapconfig LDAPConfig ldapusers orgfileextwhitelist orggroup orguser userrole; do
   psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
 done
