447ee8131567edbc7a67655f759e110b8250efe2
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-11 18:43:52 +0200

    add groupuser migrations

--- liquibase/pgsql-prepare.sh
+++ liquibase/pgsql-prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in groupuser GroupUser groupdnpair groupstructure GroupStructure ldapconfig LDAPConfig ldapusers organization orgfileextwhitelist orggroup orguser userrole; do
+for table in groupdnpair groupstructure GroupStructure ldapconfig LDAPConfig ldapusers organization orgfileextwhitelist orggroup orguser userrole; do
   psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
 done
