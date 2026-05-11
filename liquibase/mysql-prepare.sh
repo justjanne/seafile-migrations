2860208375f7651c5826cf39cb4c4b8cff9d12e0
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-11 20:17:20 +0200

    add groupdnpair

--- liquibase/mysql-prepare.sh
+++ liquibase/mysql-prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in GroupDNPair LDAPConfig LDAPUsers Organization OrgFileExtWhiteList OrgGroup OrgUser UserRole; do
+for table in LDAPConfig LDAPUsers Organization OrgFileExtWhiteList OrgGroup OrgUser UserRole; do
   mariadb -h 127.0.0.1 -u root -ppassword $1 -e "drop table if exists \`$table\`" || true
 done
