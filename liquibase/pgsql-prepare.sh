4a6ab5f7284d713d6a672511f7158b676cafba5a
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-12 00:00:57 +0200

    add orgfileextwhitelist migrations

--- liquibase/pgsql-prepare.sh
+++ liquibase/pgsql-prepare.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in orgfileextwhitelist; do
-  psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
-done
+true
