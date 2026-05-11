a11f683c44e284a79c2a4b46ddde60aa87328f3b
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-11 23:54:10 +0200

    add ldapconfig migrations

--- liquibase/pgsql-prepare.sh
+++ liquibase/pgsql-prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in ldapconfig LDAPConfig orgfileextwhitelist userrole; do
+for table in orgfileextwhitelist userrole; do
   psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
 done
