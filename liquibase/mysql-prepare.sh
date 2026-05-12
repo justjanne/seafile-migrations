1d2ac5845e5630fbca964d80ee4cc6fab441a18d
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-12 11:42:33 +0200

    add sharedrepo migrations

--- liquibase/mysql-prepare.sh
+++ liquibase/mysql-prepare.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 for table in OrgQuota OrgUserQuota RepoUserToken \
-               SharedRepo UserQuota WebAP FileLockTimestamp FolderGroupPerm FolderPermTimestamp FolderUserPerm \
+               UserQuota WebAP FileLockTimestamp FolderGroupPerm FolderPermTimestamp FolderUserPerm \
                GarbageRepos GCID LastGCID OrgDownloadRateLimit OrgGroupRepo OrgInnerPubRepo OrgRepo OrgSharedRepo \
                OrgUploadRateLimit OrgUserDefaultQuota RepoFileCount RepoHistoryLimit RepoInfo RepoStorageId \
                RepoSyncError RepoTokenPeerInfo RepoTrash RepoValidSince RoleDownloadRateLimit RoleQuota \
