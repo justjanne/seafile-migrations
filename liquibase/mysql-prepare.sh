be9fbc54c569045c79527fbde7dbe95c499602fb
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-12 21:56:00 +0200

    add the remaining migrations

--- liquibase/mysql-prepare.sh
+++ liquibase/mysql-prepare.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in \
-               \
-               OrgDownloadRateLimit OrgRepo OrgSharedRepo \
-               OrgUploadRateLimit OrgUserDefaultQuota RepoStorageId \
-               RepoSyncError RoleDownloadRateLimit RoleQuota \
-               RoleUploadRateLimit UserDownloadRateLimit UserUploadRateLimit \
-               WebUploadTempFiles; do
-  mariadb -h 127.0.0.1 -u root -ppassword $1 -e "drop table if exists \`$table\`" || true
-done
+#for table in ; do
+#  mariadb -h 127.0.0.1 -u root -ppassword $1 -e "drop table if exists \`$table\`" || true
+#done
+true
--- liquibase/pgsql-prepare.sh
+++ liquibase/pgsql-prepare.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for table in \
-               \
-               OrgDownloadRateLimit OrgRepo OrgSharedRepo \
-               OrgUploadRateLimit OrgUserDefaultQuota RepoStorageId \
-               RepoSyncError RoleDownloadRateLimit RoleQuota \
-               RoleUploadRateLimit UserDownloadRateLimit UserUploadRateLimit \
-               WebUploadTempFiles; do
-  psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
-done
+#for table in ; do
+#  psql postgres://postgres:password@localhost/$1 -c "drop table if exists \"$table\"" || true
+#done
+true
