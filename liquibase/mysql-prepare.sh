0744a23966963a87551de20dd8a52298e0c41737
Janne Mareike Koschinski <mail@justjanne.de>
2026-05-12 12:27:54 +0200

    add repovalidsince migrations

--- liquibase/mysql-prepare.sh
+++ liquibase/mysql-prepare.sh
@@ -5,7 +5,7 @@ for table in \
                FileLockTimestamp FolderGroupPerm FolderPermTimestamp FolderUserPerm \
                GarbageRepos GCID LastGCID OrgDownloadRateLimit OrgGroupRepo OrgInnerPubRepo OrgRepo OrgSharedRepo \
                OrgUploadRateLimit OrgUserDefaultQuota RepoFileCount RepoInfo RepoStorageId \
-               RepoSyncError RepoTokenPeerInfo RepoTrash RepoValidSince RoleDownloadRateLimit RoleQuota \
+               RepoSyncError RepoTokenPeerInfo RepoTrash RoleDownloadRateLimit RoleQuota \
                RoleUploadRateLimit SeafileConf SystemInfo UserShareQuota UserDownloadRateLimit UserUploadRateLimit \
                VirtualRepo WebUploadTempFiles FileLocks; do
   mariadb -h 127.0.0.1 -u root -ppassword $1 -e "drop table if exists \`$table\`" || true
